0602c4d6be347f72eb93b9030cc094ef1e1dcf5a
foreverkb24 <wangtongkb24@gmail.com>
2013-09-24 07:02:20 +0800

    9.23

--- src/com/whumap/activity/ContentFrameActivity.java
+++ src/com/whumap/activity/ContentFrameActivity.java
@@ -1,50 +1,175 @@
 package com.whumap.activity;
 
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuItem;
 import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
-import com.whumap.fragment.MenuListFragment;
+import com.jeremyfeinstein.slidingmenu.lib.app.SlidingFragmentActivity;
+import com.whumap.fragment.NewsFragment;
 import com.whumap.fragment.TextFragment;
 import com.whumap.map.MyMapFragment;
+import com.whumap.util.CountDownDate;
 
 import android.app.Fragment;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
-public class ContentFrameActivity extends BaseActivity{
+public class ContentFrameActivity extends SlidingFragmentActivity{
 
-	public ContentFrameActivity() {
-		super(R.string.app_name);
-	}
-
+	/** slidingMenu */
+	private SlidingMenu slidingMenu;
+	/** slidingMenu左侧菜单 */
+	private ListView leftMenu;
+	/** 地图fragment*/
+	private MyMapFragment myMapFragment ; 
+	/** 文字fragment*/
+	private TextFragment textFragment ;
+	/** 新闻fragment*/
+	private NewsFragment newsFragment;
+	/** 当前显示的fragment*/
 	private Fragment currentFragment ;
+	/** 倒计时日期*/
+	private TextView countDownDate;
+	/** 左侧菜单的父容器，用来修改倒计时按钮*/
+	private LinearLayout ll;
+	/** 倒计时容器*/
+	private RelativeLayout rl;
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setTitle("Demo");
-	/*	if(savedInstanceState != null) {
-			currentFragment = getFragmentManager().getFragment(savedInstanceState, "fragment");
-		}
-		if(currentFragment == null) {
-			currentFragment = new MyMapFragment();
-		}*/
-		currentFragment = new MyMapFragment();
-		setContentView(R.layout.content_frame);
-		FragmentTransaction ft = getFragmentManager().beginTransaction();
-		ft.replace(R.id.content_frame, currentFragment);
-		ft.commit();
 		setBehindContentView(R.layout.menu_frame);
-		getFragmentManager()
-		.beginTransaction()
-		.replace(R.id.menu_frame, new MenuListFragment()).commit();
-		getSlidingMenu().setTouchModeAbove(SlidingMenu.TOUCHMODE_NONE);
+		setContentView(R.layout.content_frame);
+		init();
+		setListener();
 	}
 	
-
-	@Override
-	protected void onSaveInstanceState(Bundle outState) {
+	/**
+	 * 所有初始化内容，包括初始化slidingMenu,
+	 * 初始化显示内容，以及初始化控件
+	 */
+	private void init() {
 		
-		super.onSaveInstanceState(outState);
+		initContent();
+		initSlidingMenu();
+		findViewById();
+		initLeftMenu();
+		initCountDownDay();
 	}
 	
+	/**
+	 * 初始化所需的所有控件
+	 */
+	private void findViewById() {
+		leftMenu = (ListView)findViewById(R.id.menulist);
+		countDownDate = (TextView)findViewById(R.id.count_down_days);
+		ll = (LinearLayout)findViewById(R.id.menu_frame);
+		rl = (RelativeLayout)findViewById(R.id.count_down_container);
+	}
+	
+	/**
+	 * 初始化刚进入时显示的内容
+	 */
+	private void initContent()  {
+		
+		myMapFragment = new MyMapFragment();
+		FragmentTransaction ft = getFragmentManager().beginTransaction();
+		ft.replace(R.id.content_frame, myMapFragment);
+		currentFragment = myMapFragment;
+		ft.commit();
+	}
+	
+	/**
+	 * 初始化左侧菜单列表
+	 */
+	private void initLeftMenu()	 {
+		
+		String[] functions = getResources().getStringArray(R.array.function_list);
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1,android.R.id.text1,functions);
+		leftMenu.setAdapter(adapter);
+	}
+	
+	/**
+	 * 初始化slidingmenu
+	 */
+	private void initSlidingMenu() {
+		
+		slidingMenu = getSlidingMenu();
+		slidingMenu.setMode(SlidingMenu.LEFT);
+		slidingMenu.setShadowDrawable(R.drawable.shadow);
+		slidingMenu.setShadowWidthRes(R.dimen.shadow_width);
+		slidingMenu.setBehindOffsetRes(R.dimen.slidingmenu_offset);
+		slidingMenu.setFadeDegree(0.35f);
+		slidingMenu.setTouchModeAbove(SlidingMenu.TOUCHMODE_NONE);
+		getSupportActionBar().setHomeButtonEnabled(true);
+	}
+	
+	
+	/**
+	 * 计算倒计时日并显示
+	 */
+	private void initCountDownDay() {
+		
+		CountDownDate date = new CountDownDate(2013, 11, 29);
+		int days = date.countDownDate();
+		if(days != 0) {
+			
+			countDownDate.setText(days+"");
+		} else {
+		
+			ll.removeView(rl);
+		}
+		
+	}
+	
+	/**
+	 * 设置监听
+	 */
+	private void setListener() {
+		
+		leftMenu.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+					long arg3) {
+				
+				switch(arg2) {
+				case 0:
+					if(myMapFragment == null) {
+						myMapFragment = new MyMapFragment();
+					}
+					switchContent(myMapFragment);
+					break;
+				case 1:
+					if(textFragment == null) {
+						textFragment = new TextFragment();
+					}
+					switchContent(textFragment);
+					break;
+				case 2:
+					if(newsFragment == null) {
+						newsFragment = new NewsFragment();
+					}
+					switchContent(newsFragment);
+					break;
+				default :
+						break;
+				}
+			}
+			
+		});
+	}
+	/**
+	 * 切换fragment
+	 * @param fragment
+	 */
 	public void switchContent(Fragment fragment) {
 		if(currentFragment != fragment) {
 			FragmentTransaction ft = getFragmentManager().beginTransaction();
@@ -55,9 +180,24 @@ public class ContentFrameActivity extends BaseActivity{
 			}
 			currentFragment = fragment;
 		}
-		/*
-		ft.add(R.id.content_frame, fragment);
-		ft.commit(); */
 		getSlidingMenu().showContent();
+	}
+	
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		
+		switch(item.getItemId()) {
+		case  android.R.id.home :
+			toggle();
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getSupportMenuInflater().inflate(R.menu.menu, menu);
+		return true;
 	}
 }
---
+++ src/com/whumap/util/CountDownDate.java
@@ -0,0 +1,126 @@
+package com.whumap.util;
+
+import java.util.Calendar;
+
+public class CountDownDate {
+
+	/** 指定倒计时的结束时间，年，月，日*/
+	private int year ; 
+	private int month ; 
+	private int day;
+	/** 当前系统时间,年，月，日 */
+	private int currentYear;
+	private int currentMonth;
+	private int currentDay;
+
+	/**
+	 * 在构造函数中初始化倒计时时间
+	 * @param year
+	 * @param month  系统默认月份从0开始
+	 * @param day
+	 */
+	public CountDownDate(int year , int month , int day) {
+		
+		this.year = year; 
+		this.month = month -1 ;
+		this.day = day;
+		
+	}
+
+	/**
+	 * 初始化当前时间
+	 */
+	private void initCurrentDate() {
+		
+		Calendar c = Calendar.getInstance();
+		currentYear = c.get(Calendar.YEAR);
+		currentMonth = c.get(Calendar.MONTH);
+		currentDay = c.get(Calendar.DAY_OF_MONTH);
+	}
+	
+	/**
+	 * 计算当前时间与倒计时时间的天数
+	 * @return
+	 */
+	public int countDownDate() {
+		
+		initCurrentDate();
+		int days = 0;
+		int daysRemainOfMonth = daysOfMonth(currentMonth) - currentDay;
+		if(currentYear == year) {
+			if(month == currentMonth ) {
+				days = day - currentDay;
+			} else if(month - currentMonth == 1) {
+				
+				days = daysRemainOfMonth + day;
+			} else {
+				days = daysRemainOfMonth + day + daysBetweenMonths(year, currentMonth, month);
+			}
+		} else {
+			
+		}
+		return days;
+	}
+	/**
+	 * 判断是否为闰年
+	 * @return
+	 */
+	private boolean isBissextile(int year) {
+		
+		return (year%4 == 0 && year%400 == 0 && year%100 != 0) ;
+		
+	}
+	
+	/**
+	 * 计算某年两个月之间的时间,月份之间至少需要间隔一个月
+	 * @param year
+	 * @param firstMonth
+	 * @param secondMonth
+	 * @return
+	 */
+	private int daysBetweenMonths(int year , int firstMonth , int secondMonth) {
+		int days = 0;
+			firstMonth++;
+			secondMonth--;
+		while(firstMonth <= secondMonth) {
+			days += daysOfMonth(firstMonth -1);
+			firstMonth++;
+		}	
+		return days;
+	}
+	
+	/**
+	 * 获取指定月的天数
+	 * @param month
+	 * @return
+	 */
+	private int daysOfMonth(int month){
+		
+		int daysOfMonth = 0;
+			switch(month) {
+			case 0:
+			case 2:
+			case 4:
+			case 6:
+			case 7:
+			case 9:
+			case 11:
+				daysOfMonth += 31;
+				break;
+			case 1:
+				if(isBissextile(year)) {
+					daysOfMonth += 29;
+				} else {
+					daysOfMonth += 28;
+				}
+				break;
+			case 3:
+			case 5:
+			case 8:
+			case 10:
+				daysOfMonth += 30;
+				break;
+			}
+		return daysOfMonth;
+	}
+}
